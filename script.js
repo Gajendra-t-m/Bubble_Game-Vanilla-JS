f2c2392284dae3a5753a6247de1f73ea6334f2b0
Gajendra <gajendratm05@gmail.com>
2023-09-17 17:09:44 +0530

    logic to get new hitnuber and to update the score is completed

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+let timer = 60;
+let hitrn = 0;
+let score = 0
+
 const makebubble = () => {
   let randomNum = "";
-  for (let i = 1; i <= 170; i++) {
+  for (let i = 1; i <= 168; i++) {
     let rn = Math.floor(Math.random() * 10);
     randomNum += `<div class="bubble">${rn}</div>`;
   }
@@ -8,17 +12,39 @@ const makebubble = () => {
 };
 
 const runtimer = () => {
-  let timer = 60;
   const timerInt = setInterval(() => {
     if (timer > 0) {
       timer--;
       document.querySelector("#timerval").textContent = timer;
-    }
-    else{
-        clearInterval(timerInt);
+    } else {
+      clearInterval(timerInt);
+      document.querySelector("#bottombar").innerHTML = `<button id="refresh">Game Over Click to Restart</button> ` ;
+      document.querySelector("#refresh").addEventListener("click", ()=>{
+        location.reload();
+      })
     }
   }, 1000);
 };
 
+const newhitnumber = () => {
+  hitrn = Math.floor(Math.random() * 10);
+  document.querySelector("#hitnum").textContent = hitrn;
+};
+
+const increasescore = () => {
+  score += 10;
+  document.querySelector("#scoreval").textContent = score;
+};
+
+document.querySelector("#bottombar").addEventListener("click", (details) => {
+  let clickedNum = Number(details.target.textContent);
+  if (clickedNum === hitrn) {
+    increasescore();
+    makebubble();
+    newhitnumber();
+  }
+});
+
+newhitnumber();
 runtimer();
 makebubble();
