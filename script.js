6152bec3a544a2f706e5bf9f1ae86f9097581f43
Gajendra <gajendratm05@gmail.com>
2023-09-17 13:52:22 +0530

    timer logic completed

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const makebubble =()=>{
-    let randomNum = "";
-    for (let i=1; i<=170; i++){
-        let rn = Math.floor(Math.random()*10);
-        randomNum += `<div class="bubble">${rn}</div>`;
-    }
-    document.querySelector("#bottombar").innerHTML = randomNum;
-}
+const makebubble = () => {
+  let randomNum = "";
+  for (let i = 1; i <= 170; i++) {
+    let rn = Math.floor(Math.random() * 10);
+    randomNum += `<div class="bubble">${rn}</div>`;
+  }
+  document.querySelector("#bottombar").innerHTML = randomNum;
+};
 
+const runtimer = () => {
+  let timer = 60;
+  const timerInt = setInterval(() => {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerval").textContent = timer;
+    }
+    else{
+        clearInterval(timerInt);
+    }
+  }, 1000);
+};
+
+runtimer();
 makebubble();
